26620a7bf00c98fee9577d1f753e9ec5ce0959fd
pythonDev161221 <ormonkenenbaev@gmail.com>
2025-07-09 13:36:17 +0600

    message (note) from zammad to telegram app is sending well

--- zammad_tg_bot/chatbot/urls.py
+++ zammad_tg_bot/chatbot/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # This URL will be something like: https://<ngrok_domain>/telegram/webhook/
     path('webhook/', views.telegram_webhook, name='telegram_webhook'),
-    path('zammad-events/', views.zammad_webhook, name='zammad_webhook'),
+    path('webhook/zammad/', views.zammad_webhook, name='zammad_webhook'),
+    path('test/', views.test_endpoint, name='test_endpoint'),
 ]
