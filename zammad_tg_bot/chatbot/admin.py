d06243f379b7a3218d5ec62951f798b3697ccb64
pythonDev161221 <ormonkenenbaev@gmail.com>
2025-07-23 12:21:49 +0600

    added all models to admin panel

--- zammad_tg_bot/chatbot/admin.py
+++ zammad_tg_bot/chatbot/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import TelegramBot, Customer, OpenTicket
 
-# Register your models here.
+
+@admin.register(TelegramBot)
+class TelegramBotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'token', 'zammad_group')
+    list_filter = ('zammad_group',)
+    search_fields = ('name', 'token')
+    readonly_fields = ('token',)
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'telegram_bot')
+    list_filter = ('telegram_bot',)
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(OpenTicket)
+class OpenTicketAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id', 'bot', 'customer', 'zammad_ticket_number', 'created_at')
+    list_filter = ('bot', 'created_at')
+    search_fields = ('telegram_id', 'zammad_ticket_number')
+    readonly_fields = ('created_at',)
